8c9e581b244b5b4daaff5ce242780c1462e080f8
marco <msartorelli@gmail.com>
2021-11-03 16:07:50 -0300

    Ajustes Importação

--- app/src/main/java/com/exmachina/pokemoncompose/compose/PokemonComponents.kt
+++ app/src/main/java/com/exmachina/pokemoncompose/compose/PokemonComponents.kt
@@ -16,11 +16,9 @@
 
 package com.exmachina.pokemoncompose.compose
 
-import androidx.annotation.StringRes
 import androidx.compose.animation.*
 import androidx.compose.animation.core.TweenSpec
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -32,9 +30,7 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import com.exmachina.pokemoncompose.model.PokemonEntity
